117a0e21f1f9b6de180f642a08091957de737521
Alfie Driver <modernelm@hotmail.co.uk>
2014-10-02 09:58:50 +0100

    This is an interactive *10 input box that will watch for changes and update the view ASAP

---
+++ AngularJS/Simple Calculator/main.js
@@ -0,0 +1,11 @@
+function StartUpController($scope) {
+    $scope.funding = {startingEstimate: 0};
+
+    computeNeeded = function(){
+        $scope.funding.needed = $scope.funding.startingEstimate*10;
+    };
+
+    $scope.$watch('funding.startingEstimate',computeNeeded); //$watch is an Angular Expression 
+}
+
+console.log("hey");
